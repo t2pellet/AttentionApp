de884918b32f7f6257fc2f1bc0542304fec1766c
daPoseidonGuy <pelletiertenzin2000@gmail.com>
2020-08-03 03:58:21 -0400

    Small tweaks

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,38 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
 admin.initializeApp();
-const bucket = admin.storage().bucket();
 
 exports.updateBoyfriend = functions.https.onCall((data, ctx) => {
-    bucket.file('boyfriend').save(data);
-});
-exports.updateGirlfriend = functions.https.onCall((data, ctx) => {
-    bucket.file('girlfriend').save(data);
+    const payload = {
+        topic: "girlfriend",
+        data: {
+            title: "Setup Complete",
+            body: "Couple configured",
+            type: "boyfriend"
+        }
+    }
+    admin.messaging().send(payload);
 });
 exports.requestAttention = functions.https.onCall((data, ctx) => {
-    const file = bucket.file('boyfriend');
-    file.download((err, contents) => {
-        if (err) {
-            console.log(err.stack);
+    const payload = {
+        topic: "boyfriend",
+        data: {
+            title: "Attention Requested",
+            body: "Fedora has requested your attention",
+            type: "attention"
         }
-        const payload = {
-            data: {
-                type: "attention",
-                title: "Attention Requested",
-                body: "Fedora has requested your attention"
-            }
-        }
-        admin.messaging().sendToDevice(contents.toString(), payload);
-    });    
+    }
+    admin.messaging().send(payload);  
     return("Attention requested!");
 });
 exports.acknowledge = functions.https.onCall((data, ctx) => {
-    const file = bucket.file('girlfriend');
-    file.download((err, contents) => {
-        if (err) {
-            console.log(err.stack);
+    const payload = {
+        topic: "girlfriend",
+        data: {
+            title: "Request Acknowledged",
+            body: "Tenzin has acknowledged your request",
+            type: "acknowledge"
         }
-        const payload = {
-            data: {
-                type: "acknowledge",
-                title: "Request Acknowledged",
-                body: "Tenzin has acknowledged your request"
-            }
-        }
-        admin.messaging().sendToDevice(contents.toString(), payload);
-    })
+    }
+    admin.messaging().send(payload);
 });
