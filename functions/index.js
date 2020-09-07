cd4005f5e48d0cbf949872ea80fbaf14f4ec6b50
daPoseidonGuy <pelletiertenzin2000@gmail.com>
2020-09-07 13:41:13 -0400

    Generalized app so multiple pairs can use it

--- functions/index.js
+++ functions/index.js
@@ -2,51 +2,61 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.setToken = functions.database.ref('boyfriend').onWrite((snapshot, context) => {
-    const payload = {
-        topic: "setup",
-        data: {
-            title: "Setup Complete",
-            body: "Couple configured",
-            type: "setup"
-        }
-    }
-    admin.messaging().send(payload);
-    return 0;
-});
-exports.requestAttention = functions.https.onCall((data, ctx) => {
-    admin.database().ref('boyfriend').once('value')
+exports.activate = functions.https.onCall((data, ctx) => {
+    admin.database().ref(data + '/girlfriend').once('value')
         .then(data => {
             const payload = {
                 data: {
-                    title: "Attention Requested",
-                    body: "Fedora has requested your attention",
-                    type: "attention"
+                    title: "Setup Complete",
+                    body: "Couple configured",
+                    type: "setup"
                 }
             }
             const options = {
                 priority: 'high'
             }
+            console.log(data.val());
             admin.messaging().sendToDevice(data.val(), payload, options);
             return 0;
         })
         .catch(error => console.log(error));
+});
+
+exports.requestAttention = functions.https.onCall((data, ctx) => {
+    admin.database().ref(data).once('value').then(snapshot => {
+        const token = snapshot.child('boyfriend').val();
+        const name = snapshot.child('girlfriendName').val();
+        const payload = {
+            data: {
+                type: "attention",
+                name:  name,
+            }
+        }
+        const options = {
+            priority: 'high'
+        }
+        admin.messaging().sendToDevice(token, payload, options);
+        return 0;
+    })
+    .catch(error => console.log(error));
     return "Attention Requested!";
 });
+
 exports.acknowledge = functions.https.onCall((data, ctx) => {
-    admin.database().ref('girlfriend').once('value')
-    .then(data => {
+    admin.database().ref(data).once('value').then(snapshot => {
+        const token = snapshot.child('girlfriend').val();
+        const name = snapshot.child('boyfriendName').val();
         const payload = {
             data: {
                 title: "Request Acknowledged",
-                body: "Tenzin has acknowledged your request",
+                body: name + " has acknowledged your request",
                 type: "acknowledge"
             }
         }
         const options = {
             priority: 'high'
         }
-        admin.messaging().sendToDevice(data.val(), payload, options);
+        admin.messaging().sendToDevice(token, payload, options);
         return 0;
     })
     .catch(error => console.log(error));
